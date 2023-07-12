670734d594c199d974dfd6f8ba13cb0cc26e6d97
Vladislav Peev <vladislavpeev9412@gmail.com>
2023-07-12 22:46:49 +0300

    added register async thunk to auth slice

--- src/features/authSlice.tsx
+++ src/features/authSlice.tsx
@@ -1,30 +1,29 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { post } from "../api/api";
 
-const initialState = { isLoading: false, userData: null };
-const url = "http://localhost:5000/api/v1/accounts/login"
+const initialState: { isLoading: boolean; userData: {} | null } = {
+  isLoading: false,
+  userData: null,
+};
+const loginURL: string = "accounts/login";
 
-export const login = createAsyncThunk('auth/login',async (payload) => {
-  const response = await fetch(url,{
-    method: "POST",
-    headers:{"Content-Type":"application/json"},
-    body:JSON.stringify(payload)
-})
-return response.json();
-})
+export const login = createAsyncThunk("auth/login", async (payload) => {
+  const response = await post(loginURL, payload);
+  return response;
+});
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   extraReducers: {
-  [login.pending]:(state) => {
-    state.isLoading = true;
-  },
-  [login.fulfilled]:(state,action) => {
-    state.isLoading = false;
-    state.userData = action.payload;
-  }
+    [login.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [login.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.userData = action.payload;
     },
   },
-);
+});
 
 export default authSlice.reducer;
