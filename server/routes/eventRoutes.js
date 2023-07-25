47460f0c9b577b66cd450fe69fb2fad99463da03
Vladislav Peev <vladislavpeev9412@gmail.com>
2023-07-26 00:01:13 +0300

    add event routes

---
+++ server/routes/eventRoutes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getGlobalEvents,
+  addGlobalEvent,
+} = require("../controllers/eventController");
+
+//get events
+router.get("/", async (req, res) => {
+  try {
+    const result = await getGlobalEvents();
+    res.json({ ok: true, result });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ ok: false, message: "Internal server error!" });
+  }
+});
+
+//get
+router.post("/", async (req, res) => {
+  const { title, description, date, start, end } = req.body;
+  const dentistId = req.account.id;
+
+  try {
+    await addGlobalEvent({ title, description, date, start, end }, dentistId);
+    return res.status(201).json({ ok: true, message: "Event added successfully!" });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ ok: false, message: "Internal server error!" });
+  }
+});
+
+module.exports = router;
