b7e9a5de25c9ae2878d690d49c8e78462c0db828
Vladislav Peev <vladislavpeev9412@gmail.com>
2023-08-04 00:41:31 +0300

    created custom error class and function for identifying it on faulty responses

---
+++ server/utils/customError.js
@@ -0,0 +1,20 @@
+class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ValidationError";
+  }
+}
+
+const rejectResponse = (res, error) => {
+  let code = 500;
+  let message = "Internal server error!";
+
+  if (error instanceof ValidationError) {
+    code = 400;
+    message = error.message;
+  }
+
+  return res.status(code).json({ ok: false, message });
+};
+
+module.exports = { ValidationError, rejectResponse };
