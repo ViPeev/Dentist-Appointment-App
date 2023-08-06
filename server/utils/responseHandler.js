af3c1bd8b0c8526a0d143014ff3ac60d5e56fd0f
Vladislav Peev <vladislavpeev9412@gmail.com>
2023-08-06 23:48:42 +0300

    created a generalized function for handling responses, keeping all endpoints a little bit drier

---
+++ server/utils/responseHandler.js
@@ -0,0 +1,39 @@
+const { ValidationError } = require("./errorClass");
+
+const rejectResponse = (res, error) => {
+  let code = 500;
+  let message = "Internal server error!";
+
+  if (error instanceof ValidationError) {
+    [message, code] = error.message.split(" - ");
+  }
+
+  return res.status(code).json({ ok: false, message });
+};
+
+const responseHandler = async ({
+  res,
+  controller,
+  deps,
+  statusCode,
+  hasDataTransfer,
+  message,
+}) => {
+  const payload = { ok: true };
+
+  try {
+    if (hasDataTransfer) {
+      const result = await controller(...deps);
+      payload.result = result;
+    } else {
+      await controller(...deps);
+      payload.message = message;
+    }
+    
+    return res.status(statusCode || 200).json(payload);
+  } catch (error) {
+    return rejectResponse(res, error);
+  }
+};
+
+module.exports = responseHandler;
