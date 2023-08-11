af01842ebe7a18ce6534a52cad9a3cbd977e0d73
Vladislav Peev <vladislavpeev9412@gmail.com>
2023-08-11 20:46:50 +0300

    modal component refactored

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,17 +4,13 @@ import { useNavigate } from "react-router-dom";
 import { login } from "../features/authSlice";
 import { authSelector } from "../utils/selectors";
 import { loginRoutes } from "../utils/roles";
-import Backdrop from "../layouts/Backdrop";
+import ModalWrapper from "../layouts/ModalWrapper";
 
-const Login = (): JSX.Element => {
-  return (
-    <Backdrop>
-      <LoginForm />
-    </Backdrop>
-  );
+const Login: React.FC = () => {
+  return <ModalWrapper Form={LoginForm} />;
 };
 
-const LoginForm: React.FC = () => {
+const LoginForm: React.FC = ({ children }) => {
   const [formData, setFormData] = useState({ email: "", password: "" });
   const dispatch = useDispatch();
   const authData = useSelector(authSelector);
@@ -38,70 +34,69 @@ const LoginForm: React.FC = () => {
   };
 
   return (
-    <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-3 py-6 lg:px-4">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Login
-          </h2>
-        </div>
+    <div className="flex min-h-full relative flex-1 flex-col justify-center px-3 py-6 lg:px-4">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Login
+        </h2>
+      </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-2" onSubmit={submitHandler}>
-            <div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-2" onSubmit={submitHandler}>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                value={formData.email}
+                onChange={changeHandler}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center justify-between">
               <label
-                htmlFor="email"
+                htmlFor="password"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Email address
+                Password
               </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  value={formData.email}
-                  onChange={changeHandler}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
-                />
-              </div>
             </div>
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Password
-                </label>
-              </div>
-              <div className="mt-2 mb-10">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  value={formData.password}
-                  onChange={changeHandler}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+            <div className="mt-2 mb-10">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                value={formData.password}
+                onChange={changeHandler}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+              />
             </div>
-            <div className="mb-4">
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600 transition-colors duration-200"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
+          </div>
+          <div className="mb-4">
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600 transition-colors duration-200"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
-    </>
+      {children}
+    </div>
   );
 };
 
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -6,22 +6,18 @@ import { register } from "../features/authSlice";
 import { registerRole } from "../utils/types";
 import { loginRoutes } from "../utils/roles";
 import Select from "../components/Misc/Select";
-import Backdrop from "../layouts/Backdrop";
+import ModalWrapper from "../layouts/ModalWrapper";
 
 const roles: registerRole[] = [
   { id: 3, name: "looking for a dentist" },
   { id: 2, name: "a dentist" },
 ];
 
-const Register = (): JSX.Element => {
-  return (
-    <Backdrop>
-      <RegisterForm />
-    </Backdrop>
-  );
+const Register: React.FC = () => {
+  return <ModalWrapper Form={RegisterForm} />;
 };
 
-const RegisterForm: React.FC = () => {
+const RegisterForm: React.FC = ({ children }) => {
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -54,137 +50,135 @@ const RegisterForm: React.FC = () => {
 
   const submitHandler = (e: SyntheticEvent<HTMLFormElement, SubmitEvent>) => {
     e.preventDefault();
-    console.log(formData);
     dispatch(register(formData));
   };
 
   return (
-    <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-3 py-6 lg:px-4">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Register
-          </h2>
-        </div>
+    <div className="flex relative min-h-full flex-1 flex-col justify-center px-3 py-6 lg:px-4">
+      {children}
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Register
+        </h2>
+      </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-2" onSubmit={submitHandler}>
-            <div className="flex flex-row gap-3">
-              <span className="w-1/2">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  First Name
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="firstName"
-                    name="firstName"
-                    type="text"
-                    autoComplete="first name"
-                    required
-                    onChange={changeHandler}
-                    value={formData.firstName}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </span>
-              <span className="w-1/2">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Last Name
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="lastName"
-                    name="lastName"
-                    type="text"
-                    autoComplete="last name"
-                    onChange={changeHandler}
-                    value={formData.lastName}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </span>
-            </div>
-            <div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-2" onSubmit={submitHandler}>
+          <div className="flex flex-row gap-3">
+            <span className="w-1/2">
               <label
                 htmlFor="email"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Email address
+                First Name
               </label>
               <div className="mt-2">
                 <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
+                  id="firstName"
+                  name="firstName"
+                  type="text"
+                  autoComplete="first name"
+                  required
                   onChange={changeHandler}
-                  value={formData.email}
+                  value={formData.firstName}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
                 />
               </div>
-            </div>
-            <Select
-              selectHandler={selectHandler}
-              selected={roles[0]}
-              values={roles}
-            />
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Password
-                </label>
-              </div>
+            </span>
+            <span className="w-1/2">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Last Name
+              </label>
               <div className="mt-2">
                 <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="password"
+                  id="lastName"
+                  name="lastName"
+                  type="text"
+                  autoComplete="last name"
                   onChange={changeHandler}
-                  value={formData.password}
+                  value={formData.lastName}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
                 />
               </div>
+            </span>
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                onChange={changeHandler}
+                value={formData.email}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+              />
             </div>
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Repeat Password
-                </label>
-              </div>
-              <div className="mt-2 mb-10">
-                <input
-                  id="repass"
-                  name="repass"
-                  type="password"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div className="mb-4">
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600 transition-colors duration-200"
+          </div>
+          <Select
+            selectHandler={selectHandler}
+            selected={roles[0]}
+            values={roles}
+          />
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Submit
-              </button>
+                Password
+              </label>
             </div>
-          </form>
-        </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="password"
+                onChange={changeHandler}
+                value={formData.password}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Repeat Password
+              </label>
+            </div>
+            <div className="mt-2 mb-10">
+              <input
+                id="repass"
+                name="repass"
+                type="password"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="mb-4">
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600 transition-colors duration-200"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
