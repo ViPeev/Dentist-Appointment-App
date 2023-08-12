3226081962d9ebff059139b6e297e064f391992b
Vladislav Peev <vladislavpeev9412@gmail.com>
2023-08-12 13:01:42 +0300

    updated types on api functions

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,20 +1,32 @@
-const deploy:boolean = false;
-export const host:string = deploy ? "" : "http://localhost:5000/api/v1/";
+const deploy: boolean = false;
+export const host: string = deploy ? "" : "http://localhost:5000/api/v1/";
 
-async function request(url, options) {
-  const response = await fetch(`${host}${url}`, options);
+type optionTypes = {
+  method: string;
+  headers: { ["X-Authorization"]?: string; ["Content-Type"]: string };
+  body: BodyInit | null | undefined;
+};
 
-  const data = await response.json();
-  return data;
+type dataType = { [index: string]: number | string; };
+
+async function request(url: string, options: optionTypes) {
+  try {
+    const response = await fetch(`${host}${url}`, options);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function getOptions(method = "get", body) {
+function getOptions(method?: string, body?: {}) {
   const options = {
-    method,
+    method: method || "get",
     headers: {},
-  };
+  } as optionTypes;
 
   const token = localStorage.getItem("authToken");
+
   if (token != null) {
     options.headers["X-Authorization"] = token;
   }
@@ -27,18 +39,21 @@ function getOptions(method = "get", body) {
   return options;
 }
 
-export async function get(url) {
+export async function get(url: string) {
   return await request(url, getOptions());
 }
 
-export async function post(url, data) {
+export async function post(url: string, data: dataType) {
   return await request(url, getOptions("post", data));
 }
 
-export async function put(url, data) {
+export async function put(
+  url: string,
+  data: dataType
+) {
   return await request(url, getOptions("put", data));
 }
 
-export async function del(url) {
+export async function del(url: string) {
   return await request(url, getOptions("delete"));
 }
